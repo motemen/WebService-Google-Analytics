30b8625c0207c370474922930084c5a30e4ef297
motemen <motemen@gmail.com>
2009-07-22 20:28:09 +0900

    initial commit

---
+++ lib/WebService/Google/Analytics.pm
@@ -0,0 +1,33 @@
+package WebService::Google::Analytics;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::Google::Analytics -
+
+=head1 SYNOPSIS
+
+  use WebService::Google::Analytics;
+
+=head1 DESCRIPTION
+
+WebService::Google::Analytics is
+
+=head1 AUTHOR
+
+motemen E<lt>motemen@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
